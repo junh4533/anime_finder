da663073645bb06561057a09ffb394323053fc55
Jun <junh4533@hstat.org>
2017-03-13 01:41:46 +0000

    add result pages

--- application_controller.rb
+++ application_controller.rb
@@ -14,10 +14,25 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get '/result1.erb' do
+    @user_anime
+    erb :result1
+  end
+
+  # get '/result2.erb' do
+  #   @user_anime
+  #   erb :result2
+  # end
+  
   post '/result' do
     @user_anime = find_anime(params[:anime_name])
     erb :result
   end
+  
+  # post '/result1' do
+  #   @user_anime 
+  #   erb :result1
+  # end
   
   
 end
---
+++ test_code.rb
@@ -0,0 +1,5 @@
+<form method="post" action="/result1">
+  
+<input type="submit" value="<%=@user_anime[:anime_titles][0]%>">
+
+</form>
