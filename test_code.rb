5ee3fdd3333a105359e431624a475ae9a7de131b
Jun <junh4533@hstat.org>
2017-03-14 23:11:30 +0000

    add index page

--- test_code.rb
+++ test_code.rb
@@ -1,15 +1,13 @@
-<form method="post" action="/result1">
-  
-<input type="submit" value="<%=@user_anime[:anime_titles][0]%>">
-
-</form>
-
-<input type="hidden" value= "<%%= @user_anime %>"  name="hidden_info">
-
-<td><%=@user_anime[:anime_titles][0]%></td>
-          <td><%=@user_anime[:anime_ratings][0]%></td>
-          <td><%=@user_anime[:anime_synopsis][0]%></td>
-          <td> <img src="<%=@user_anime[:anime_images][0]%>"> </td>
-         
-         
-         <% @result1_info %>
+ click on link results
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Results</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <% @post1 %>
+        </tr>
+      </tbody>
+    </table>
