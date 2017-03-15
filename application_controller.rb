32ea3362ede518e9b29f9f4c29a1e83c3520af09
Jun <junh4533@hstat.org>
2017-03-15 01:31:04 +0000

    add result 1 template

--- application_controller.rb
+++ application_controller.rb
@@ -11,8 +11,8 @@ require_relative 'models/model.rb'
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    erb :index
-    # erb:result1
+    # erb :index
+    erb:result1
   end
   
   post '/result' do
