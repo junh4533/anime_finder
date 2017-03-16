5e2672cbb3fcbce93356e5681210abeedeb6b165
Jun <junh4533@hstat.org>
2017-03-16 03:41:27 +0000

    completed result pages

--- application_controller.rb
+++ application_controller.rb
@@ -10,16 +10,39 @@ require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
+  # quick access
+  # get '/' do
+  #   erb :index
+  #   erb :result
+  #   erb :result1
+  #   erb :result2
+  #   erb :result3
+  #   erb :result4
+  #   erb :result5
+  # end
+  
   get '/' do
     erb :index
   end
   
+  #home button
+  get '/index' do
+    erb :index
+  end
+  
+  #previous button
+  get '/result' do
+    @user_anime = @@user_anime
+    erb :result
+  end
+  
+  #submitting results
   post '/result' do
     @@user_anime = find_anime(params[:anime_name])
     @user_anime = @@user_anime
     erb :result
   end
-  
+
   post '/result1' do
     @user_anime = @@user_anime
     erb :result1
